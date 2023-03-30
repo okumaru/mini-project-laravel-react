04c7b6b0f3c86d5d9e0f30f0a6c989b622d275a6
Andhika M. Wijaya <okumaru07@gmail.com>
2023-03-30 20:01:22 +0700

    fixbug on vite build

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/sb-admin-2.min.css',
+                'resources/css/all.min.css'
             ],
             refresh: true,
         }),
